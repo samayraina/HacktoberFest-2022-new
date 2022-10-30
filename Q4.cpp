a0efc4a824d7d5741ebc62376b30d34a2d0f4117
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2022-10-31 02:22:51 +0530

    Create Q4.cpp

---
+++ Q4.cpp
@@ -0,0 +1,7 @@
+Given an array arr[] denoting heights of N towers and a positive integer K.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+Increase the height of the tower by K
+Decrease the height of the tower by K
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
